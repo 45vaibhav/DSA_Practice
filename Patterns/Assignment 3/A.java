412bf4df8d6d4d7e01cfed78ff8c05068f558bf6
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-05 23:39:27 +0530

    A

---
+++ Patterns/Assignment 3/A.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class A {
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the size :");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+               if(j==1||j==2*i-1 || i==n/2+1){
+                System.out.print("*");
+               }else{
+                System.out.print(" ");
+               }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Enter the size :
+5
+    *
+   * *
+  *****
+ *     *
+*       *
+*/
