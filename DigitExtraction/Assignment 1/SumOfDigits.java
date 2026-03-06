4cfad7445cd2e3a68367dd10aecafd84a8778664
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-06 12:06:32 +0530

    Sum of digits

---
+++ DigitExtraction/Assignment 1/SumOfDigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+      public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number :");
+        int n=sc.nextInt();
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            n/=10;
+           sum=sum+rem;
+        }
+       System.out.println(sum);
+    } 
+}
+
+/*
+enter the number :
+543
+12
+*/
