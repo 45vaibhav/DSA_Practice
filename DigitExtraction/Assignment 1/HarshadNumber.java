c981df02cbb249ad3ce615cfa11fed37011abc07
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-09 21:46:17 +0530

    harshad number finding

---
+++ DigitExtraction/Assignment 1/HarshadNumber.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class HarshadNumber {
+      public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number : ");
+        int n=sc.nextInt();
+        int sum=0;
+        int original=n;
+       while(n!=0){
+                int rem=n%10;
+                sum=sum+rem;
+                n=n/10;
+       }
+       
+        if(original%sum==0){
+            System.out.println("Harshad number ");
+        }else{
+            System.out.println("not harshad number ");
+        }
+    }
+}
+
+/*
+enter the number : 
+18
+Harshad number
+
+enter the number : 
+16
+not harshad number 
+*/
