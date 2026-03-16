95e3bd426a48b05c68df11a106a0d25ea2c850ba
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-16 23:16:32 +0530

    Neon number

---
+++ DigitExtraction/Assignment 1/NeonNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NeonNumber {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number : ");
+        int n=sc.nextInt();
+        int sum=0;
+        int square=n*n;
+
+        while(square!=0){
+            int rem=square%10;
+            sum+=rem;
+            square/=10;
+        }
+
+        if(sum==n){
+            System.out.println("Neon number ");
+        }else{
+            System.out.println("not neon number ");
+        }
+    }
+}
+
+
+/*
+enter the number : 
+9
+Neon number 
+
+enter the number : 
+6
+not neon number 
+*/
