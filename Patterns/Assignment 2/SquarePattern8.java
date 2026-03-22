367da366fdad48cd816a2f7af9bdfc4870276e29
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-22 23:21:53 +0530

    number pattern odd

---
+++ Patterns/Assignment 2/SquarePattern8.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SquarePattern8 {
+    public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the size :");
+			int n=sc.nextInt();
+            int count=1;
+			for(int i=1;i<=n;i++){
+				for(int j=1;j<=n;j++){
+					if(i>=j){
+					System.out.print(count+" ");
+                    count+=2;
+				}else{
+					System.out.print("  ");
+				}
+			}
+		System.out.println();
+		}
+	}
+}
+
+/*
+Enter the size :
+4
+1
+3 5
+7 9 11
+13 15 17 19
+*/
