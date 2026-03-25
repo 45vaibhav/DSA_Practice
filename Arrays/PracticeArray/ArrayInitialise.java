299173de4accf35239de5355edffe60526c632c9
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-25 22:27:31 +0530

    Array initialisation

---
+++ Arrays/PracticeArray/ArrayInitialise.java
@@ -0,0 +1,8 @@
+public class ArrayInitialise {
+    public static void main(String args[]){
+
+        // This is array initialise 
+        int arr[]=new int[5];
+        System.out.println(arr);
+    }
+}
