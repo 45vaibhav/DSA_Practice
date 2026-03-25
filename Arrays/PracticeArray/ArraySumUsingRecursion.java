4f2e1c57df5a574ec610924c1c5be3288a26245d
Vaibhav <vaibhavkapde445@gmail.com>
2026-03-26 00:23:15 +0530

    finding array sum using the recursion

---
+++ Arrays/PracticeArray/ArraySumUsingRecursion.java
@@ -0,0 +1,20 @@
+public class ArraySumUsingRecursion {
+    public static void main(String args[]){
+        int arr[]={4,6,3,8};
+        int index=0;
+        int sum=fun(arr,index);
+        System.out.println( "the array sum is :"+sum);
+
+    }
+    static int fun(int arr[],int index){
+        if(index==arr.length-1){
+            return arr[index];
+        }
+
+        return arr[index]+fun(arr,index+1);
+    }
+}
+
+/*
+the array sum is :21
+*/
---
+++ Arrays/PracticeArray/PrintArrayUsingrecursion.java
@@ -0,0 +1,17 @@
+public class PrintArrayUsingrecursion {
+    public static void main(String args[]){
+        int arr[]={2,4,6,7,0};
+        int n=arr.length-1;
+            fun(arr,n);
+    }
+
+   static void fun(int []arr,int n){
+        if(n==0){
+            System.out.println(arr[n]);
+            return ;
+        }
+
+        fun(arr,n-1);
+        System.out.println(arr[n]);
+    }
+}
