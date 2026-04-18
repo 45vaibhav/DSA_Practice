abcb3cb7c5a89d7d6ccb762a6be2cf62ea89ef4c
Vaibhav <vaibhavkapde445@gmail.com>
2026-04-18 14:52:12 +0530

    Array sort by selection sort

---
+++ SortingAlgorithms/AssignMent 9/SelectionSort.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array : ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("enter the array element : ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int minIndex=0;
+
+        for(int i=0;i<n-1;i++){
+            minIndex=i;
+
+            for(int j=i+1;j<n;j++){
+                if(arr[minIndex]>arr[j]){
+                    minIndex=j;
+                }
+            }
+
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+        }
+        System.out.println("sorted array is ");
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+/*
+enter the size of array : 
+5
+enter the array element : 
+18 22 20 21 19
+sorted array is 
+[18, 19, 20, 21, 22]
+*/
