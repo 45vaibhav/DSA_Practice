4324e0ac77dacfaf38a6e1902f3b1f20c80e5d3e
Vaibhav <vaibhavkapde445@gmail.com>
2026-04-21 09:40:18 +0530

    Finding the max consecative zeros

---
+++ Arrays/PracticeArray/ConsecativeZero.java
@@ -0,0 +1,21 @@
+public class ConsecativeZero {
+    public static void main(String args[]){
+        int arr[]={1,1,0,1,1,1,1,0,0,1,1,1};
+        int max=0;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                count++;
+            }
+           if(count>0&& arr[i]==0){
+            max=count;
+            count=0;
+           }
+        }
+        System.out.println("the max consecative 0 is : "+max);
+    }
+}
+
+/*
+the max consecative 0 is : 4
+*/
