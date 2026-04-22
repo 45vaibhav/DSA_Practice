339027d8953c70a0b0d03ce1c93569abed4eb44c
Vaibhav <vaibhavkapde445@gmail.com>
2026-04-22 22:22:12 +0530

    Sorting array

---
+++ Arrays/PracticeArray/Sort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String args[]){
+        int arr[]={1,1,0,0,1,2,2,0};
+
+        for(int i=0;i<arr.length-1;i++){
+            int minIndex=i;
+
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minIndex]>arr[j]){
+                    minIndex=j;
+            }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+/*
+[0, 0, 0, 1, 1, 1, 2, 2]
+*/
--- Arrays/PracticeArray/UniqueEleUsingXOR.java
+++ Arrays/PracticeArray/UniqueEleUsingXOR.java
@@ -1,11 +1,16 @@
 public class UniqueEleUsingXOR {
     public static void main(String args[]){
-        int arr[]={1,2,3,2,1};
+        int arr[]={2,6,5,8,11};
 
         int result=0;
-
+        int target=14;
         for(int i=0;i<arr.length;i++){
+            int sec=result;
             result=result^arr[i];
+            if(result==target){
+                System.out.println(sec);
+                System.out.println(arr[i]);
+            }
         }
 
         System.out.println(result);
