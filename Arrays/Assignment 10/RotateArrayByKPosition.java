7b000803ec46ffef7c828c4a9b03a85f1bde5458
Vaibhav <vaibhavkapde445@gmail.com>
2026-05-07 00:02:46 +0530

    Array rotation by k position

---
+++ Arrays/Assignment 10/RotateArrayByKPosition.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RotateArrayByKPosition {
+     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array : ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the array Element : ");
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("enter k : ");
+        int k=sc.nextInt();
+        int ans[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            ans[((i+k)%n)]=arr[i];
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
+
+/*
+enter the size of array : 
+5
+Enter the array Element : 
+1 2 3 4 5
+enter k : 
+3
+[3, 4, 5, 1, 2]
+*/
