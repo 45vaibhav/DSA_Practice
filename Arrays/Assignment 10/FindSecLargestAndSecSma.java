3980bacffc25484e3723d59a4648bacb2890f463
Vaibhav <vaibhavkapde445@gmail.com>
2026-05-06 23:17:36 +0530

    Find second small and second large element in array

--- Arrays/Assignment 10/FindSecLargestAndSecSma.java
+++ Arrays/Assignment 10/FindSecLargestAndSecSma.java
@@ -42,4 +42,12 @@ Enter the array Element :
 5 2 8 1 9
 sec large : 8
 sec small : 2
+
+
+enter the size of array : 
+6
+Enter the array Element : 
+3 44 5 66 9 1 09
+sec large : 44
+sec small : 3
 */
